081cef03ca1c9ad0f7559af76421a3e3aa245c56
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-05-14 22:37:23 +0200

    add darkmode

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import ShowList from "./ShowList";
 import Nav from "./Nav";
 import { ShowProvider } from "./ShowContext";
-import AddShow from './AddShow';
+import AddShow from "./AddShow";
 
 function App() {
+	const [darkMode, setDarkMode] = useState(getInitialMode());
+
+	useEffect(() => {
+		localStorage.setItem("dark", JSON.stringify(darkMode));
+	}, [darkMode]);
+
+	// getInitialMode를 const 이용해서 arrow function 으로 했을 때는 오류 떴음
+
+	function getInitialMode() {
+		const isReturningUser = "dark" in localStorage;
+		const savedMode = JSON.parse(localStorage.getItem("dark"));
+		const userPrefersDark = getPrefColorScheme();
+		// if the mode was saved => dark / light
+		if (isReturningUser) {
+			return savedMode;
+		// if prefered color scheme is dark => dark
+		} else if (userPrefersDark) {
+			return true;
+		// otherwise, light
+		} else {
+			return false;
+		}
+	}
+
+	function getPrefColorScheme() {
+		if (!window.matchMedia) return;
+		return window.matchMedia("(prefers-color-scheme: dark)").matches;
+	}
+
 	return (
 		<ShowProvider>
 			<div className='App'>
-				<Nav />
-        <AddShow />
-				<ShowList />
+				<div className={darkMode ? "dark-mode" : "light-mode"}>
+					<Nav />
+					<div className='toggle-container'>
+						<button onClick={() => setDarkMode((prevMode) => !prevMode)}>
+							Toggle
+						</button>
+					</div>
+					<p>{darkMode ? "Dark Mode" : "Light Mode"}</p>
+					<AddShow />
+					<ShowList />
+				</div>
 			</div>
 		</ShowProvider>
 	);
--- src/Nav.js
+++ src/Nav.js
@@ -2,13 +2,14 @@ import React, {useContext} from "react";
 import {ShowContext} from './ShowContext';
 
 
-const Nav = () => {
+const Nav = (props) => {
     const [shows, setShows] = useContext(ShowContext);
 
 	return (
 		<div className="navBar">
             <h3>Your Name</h3>
             <p>List of Shows on Netflix: {shows.length}</p>
+			<p>{props.mode}</p>
 		</div>
 	);
 };
